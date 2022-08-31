4ff22dba420e32b888789d08ab1fc8f255231f5f
Aashutosh <ashutoshnishad2003@gmail.com>
2022-08-30 19:56:09 -0700

    Ready to heroku

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
 
-
-
 app.get("/" , (req,res)=>{
-    return res.sendFile( __dirname+"/ashu.html");
+return res.sendFile( __dirname+"/ashu.html");
 })
 
 app.listen(8000 , ()=>{
-    console.log('http://localhost:8000');
-    
+    console.log('http://localhost:8000');  
 })
